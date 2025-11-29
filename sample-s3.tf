eeccf475b733e4cd1d24f3ccd21ecf996403a873
mathphysicstutor76 M <mathphysicstutor76@gmail.com>
2025-11-29 12:27:01 -0500

    Update S3 config

--- sample-s3.tf
+++ sample-s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-test-bucket-${data.aws_caller_identity.current.account_id}"
+  # Missing encryption - will fail CMMC compliance
 }
 
 data "aws_caller_identity" "current" {}
